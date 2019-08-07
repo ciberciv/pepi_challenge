220c6b6ae4650287f407ae295ff9a118376d11dd
ciberciv <vcasasm@hotmail.es>
2019-08-07 14:41:27 +0200

    Add terminal functionality to run the script

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import json
+import sys
 from TripGraph import TripGraph
 
 
-with open("exercise1.json") as file:
+# This takes care of arguments to run the script from the command line. Default is "map.json" and 7 days.
+# You can specify only the file or both file and number of days too.
+if len(sys.argv) == 1:
+    filename = "map.json"
+    maxDays = 7
+elif len(sys.argv) == 2:
+    filename = sys.argv[1]
+    maxDays = 7
+else:
+    filename = sys.argv[1]
+    maxDays = int(sys.argv[2])
+
+with open(filename) as file:
     data = json.load(file)
 
 cities = data["cities"]
 connections = data["connections"]
 
-pepi = TripGraph(cities, connections, 5)
+pepi = TripGraph(cities, connections, maxDays)
 
 print(pepi.bestPath)
